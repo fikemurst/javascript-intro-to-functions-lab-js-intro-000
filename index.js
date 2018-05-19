15200921b2133be14ea002d05754738fc83a9531
michael furst <michael@furst.us>
2018-05-19 21:31:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return("I can't hear you!")
-  if ('string' = uppercase) {
+  if (string = uppercase) {
     return("YES INDEED!")
   }
   if (string = 'I love you, Grandma.') {
