7c5d31e5555cfda61405c791930219ef756b4cbe
michael furst <michael@furst.us>
2018-05-19 21:34:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,5 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   return('I can\'t hear you!')
-  if (string = 'HELLO') {
-    return("YES INDEED!")
-  }
-  if (string = 'I love you, Grandma.') {
-    return("I love you, too.")
-  }
-
+  
 }
